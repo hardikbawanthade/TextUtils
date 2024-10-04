eec5d420b4c3d61fe683628d091d0f180726451c
hardikbawanthade <144665512+hardikbawanthade@users.noreply.github.com>
2024-10-04 17:54:49 +0530

    Updates

---
+++ precache-manifest.a8ab7ba078fced417894e3fd778f171a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cedfb1a99fa236424b4c25e42d0f8b8d",
+    "url": "/Textutils-React/index.html"
+  },
+  {
+    "revision": "4a1a2ff0fc1e96ae3ce2",
+    "url": "/Textutils-React/static/css/main.be0c93d8.chunk.css"
+  },
+  {
+    "revision": "0da3a3a7caac8e2de5c7",
+    "url": "/Textutils-React/static/js/2.a5b0c296.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/Textutils-React/static/js/2.a5b0c296.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4c22086003eed385c50",
+    "url": "/Textutils-React/static/js/3.b5bd36bd.chunk.js"
+  },
+  {
+    "revision": "4a1a2ff0fc1e96ae3ce2",
+    "url": "/Textutils-React/static/js/main.92a6d855.chunk.js"
+  },
+  {
+    "revision": "ef397ae966ff0c8afe69",
+    "url": "/Textutils-React/static/js/runtime-main.d5c0a1bd.js"
+  }
+]);
